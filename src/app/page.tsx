878600a9bb6516be65e9216d4ec665eb24417ed5
objectMaker <528154320@qq.com>
2024-06-22 02:12:44 +0800

    add query users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <> i am page</>;
+export default async function Home() {
+  const res = await fetch('http://localhost:8080/getUserList');
+  const userListInfo = await res.json();
+  return (
+    <>
+      {userListInfo?.body?.map(function (item: { Name: string; ID: string }) {
+        return (
+          <div key={item.ID}>
+            id : {item.ID} name : {item.Name}
+          </div>
+        );
+      })}
+    </>
+  );
 }
