483227f529312e600c7eefe5499ebbfac8a83b39
objectMaker <528154320@qq.com>
2024-06-26 22:23:10 +0800

    sign up style

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,7 +1,9 @@
+import SignUpForm from './components/form';
+
 export default function signUp() {
   return (
-    <>
-      <div>sign up</div>
-    </>
+    <div className="border-neutral-200-50 absolute left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-4 shadow-xl">
+      <SignUpForm></SignUpForm>
+    </div>
   );
 }
